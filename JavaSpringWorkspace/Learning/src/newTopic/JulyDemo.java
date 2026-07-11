471a898795e5b2d533a213a59551c7df2a2e88a9
AboutSatyam <programmersatyam07@gmail.com>
2026-07-11 11:21:49 +0530

    last

--- JavaSpringWorkspace/Learning/src/newTopic/JulyDemo.java
+++ JavaSpringWorkspace/Learning/src/newTopic/JulyDemo.java
@@ -1,9 +1,32 @@
 package newTopic;
 
+class maydemo {
+	void das1() {
+		System.out.println("Hello world 1");
+	}
+
+	void das2() {
+		System.out.println("Hello world 2");
+	}
+
+	void das3() {
+		System.out.println("Hello world 3");
+	}
+
+}
+
 public class JulyDemo {
 
 	public static void main(String[] args) {
-		System.out.println("HEllo world ");
+		{
+			System.out.println("HEllo world ");
+
+		}
+
+		maydemo md = new maydemo();
+		md.das1();
+		md.das2();
+		md.das3();
 
 	}
 
